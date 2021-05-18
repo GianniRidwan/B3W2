d6b32468c3a42a5d0b27e44646df0034503a296f
Gianni Ridwan <99036552@mydavinci.nl>
2021-05-18 13:54:19 +0200

    Finished GET method

---
+++ welcome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>welcome</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<h1>De ingevulde gegevens zijn:</h1><br>
+	<p>Naam: <?php echo $_GET['name']; ?><br>
+		Emailadres: <?php echo $_GET['mail']; ?></p>
+
+</body>
+</html>
